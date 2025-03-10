3b76705a934e65b9152c83d4db0e0486cca9e26c
haf-cz <josefkafka1@gmail.com>
2025-03-10 16:47:29 +0100

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CodeQLExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            Console.WriteLine("User input: " + userInput);
+        }
+    }
+}
