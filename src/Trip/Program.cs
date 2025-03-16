117622148c3e09beae1688d1ef6625d0ed9b7766
haf-cz <josefkafka1@gmail.com>
2025-03-16 18:06:03 +0100

    read any file who cares

--- src/Trip/Program.cs
+++ src/Trip/Program.cs
@@ -1,2 +1,13 @@
-﻿string userInput = Console.ReadLine();
+﻿var userInput = Console.ReadLine();
+if (userInput is null)
+{
+    Console.WriteLine("null input");
+    return -1;
+}
 Console.WriteLine($"User input:'{userInput}'");
+foreach (var line in File.ReadLines(userInput))
+{
+    Console.WriteLine(line);    
+}
+
+return 0;
